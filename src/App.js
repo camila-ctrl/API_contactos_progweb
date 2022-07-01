3df1d221ae65cff48a90d6431cb41e998b046d43
Michael Flores <mich4el.flores@gmail.com>
2022-07-01 08:46:25 -0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import {
   ThemeProvider,
   Preflight,
 } from "@xstyled/styled-components";
-import ContactsList from "./components/contacts/contacts-list";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Page404 from "./pages/404";
+import ContactForm from "./components/contacts/contact-form";
 
 const theme = {
   ...defaultTheme,
@@ -14,7 +17,14 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Preflight />
-      <ContactsList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contacts/:id" element={<ContactForm />} />
+          <Route path="/contacts/new" element={<ContactForm />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
